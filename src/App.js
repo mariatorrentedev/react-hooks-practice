c41f1cbb4ef2adb421a9426291791c7190cd9e0d
María Torrente <caritotorrente@gmail.com>
2021-03-13 12:49:37 -0500

    examples on how to useState w/different types of data.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,54 @@
-import React from "react";
+// Destructure to call the buildIn State, to not use Class Components.
+import React, { useState } from "react";
 
-function App() {
-  return <main className="App">{/* content goes here */}</main>;
-}
+/*If there is a big computation is better to
+ create a function instead of pasing the parameter directetly to the useState(), 
+ so that it is called just the first time and not every time that the component renders.
+ example: 
+
+ const initialState = () => {
+ return 10; 
+ }
+
+ .then
+
+ const x = useState(()=> initialState());
+*/
+const App = () => {
+  //Example using strings or Numbers as parameters in state.
+  const [count, setCount] = useState(10);
+  //Example using an object, value:key pairs as parameters in state.
+  const [{ countObj, countObj2 }, setCountObj] = useState({
+    countObj: 10,
+    countObj2: 30,
+  });
+
+  return (
+    <main className="App">
+      {/*  cCount = the current count(current state ) (* avoid over rendering*/}
+      <h2>Counter using strings or number in state as parameter</h2>
+      <button onClick={() => setCount((cCount) => cCount + 1)}>
+        {" "}
+        Add to the count +{" "}
+      </button>
+      <h2>{count}</h2>
+      <hr />
+      <h2>Counter using an Object in state as parameter</h2>
+      <button
+        onClick={() =>
+          setCountObj((currentState) => ({
+            ...currentState,
+            countObj: currentState.countObj + 1,
+          }))
+        }
+      >
+        {" "}
+        Add to the count +{" "}
+      </button>
+      <h2>Count 1: {countObj}</h2>
+      <h2>Count 2: {countObj2}</h2>
+    </main>
+  );
+};
 
 export default App;
